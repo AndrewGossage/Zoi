bf79c40ed23ebaa06a4ffea75fcde8e8196e2b76
Andrew Gossage <gossachfront@gmail.com>
2023-05-09 21:48:18 -0600

    improved fault tolerance of toml parsing

--- src/toml.zig
+++ src/toml.zig
@@ -7,13 +7,22 @@ const eql = std.mem.eql;
 pub fn readToml(allocator: Allocator) ![]u8 {
     return try server.read_file(tomlName, allocator);
 }
+pub fn lastDigit(buf: anytype) usize {
+    var end = buf.len - 1;
+    while (isDigit(buf[end]) == false) end -= 1;
+    return end;
+}
 
 pub fn getPort() !u16 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     var value = try readKeyValue("[server]", "port", allocator);
     defer allocator.free(value);
-    return try std.fmt.parseInt(u16, value, 0);
+    var end = lastDigit(value);
+    return std.fmt.parseInt(u16, value[0 .. end + 1], 0) catch |e| {
+        std.debug.print("failed to read value: '{s}' for port\n", .{value[0..end]});
+        return e;
+    };
 }
 
 pub fn getWorkerCount() !u16 {
@@ -21,9 +30,9 @@ pub fn getWorkerCount() !u16 {
     const allocator = gpa.allocator();
     var value = try readKeyValue("[server]", "workers", allocator);
     defer allocator.free(value);
-
-    return std.fmt.parseInt(u16, value, 0) catch |e| {
-        std.debug.print("val: {any}", .{value});
+    var end = lastDigit(value);
+    return std.fmt.parseInt(u16, value[0 .. end + 1], 0) catch |e| {
+        std.debug.print("failed to read value: '{s}' for worker count\n", .{value[0..end]});
         return e;
     };
 }
@@ -73,13 +82,14 @@ pub fn readKeyValue(section: anytype, key: anytype, allocator: Allocator) ![]u8 
     while (it.next()) |slice| {
         // check if we found the key + the proper spaceing and
         // equal sign
-        if (eql(u8, slice, key) and eql(u8, " = ", t[(it.index.? + key.len) .. it.index.? + 3 + key.len])) {
-            pos = it.index.? + 3 + key.len;
+        if (eql(u8, slice, key) and eql(u8, " =", t[(it.index.? + key.len) .. it.index.? + 2 + key.len]) and t[it.index.? - 1] == '\n') {
+            pos = it.index.? + 2 + key.len;
             found = true;
         } else if (slice[0] == '[') {
             return "";
         }
     }
+    while (t[pos] == ' ') pos += 1;
     if (!found) return "";
     var end: usize = pos;
 
